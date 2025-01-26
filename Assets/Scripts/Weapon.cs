2b35284c8d662476121c77da9e26e67a37d07fde
Feihuan Peng <ericrealtor1997@gmail.com>
2025-01-25 20:23:48 -0800

    weapon ray cast

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,30 @@
+using StarterAssets;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    StarterAssetsInputs starterAssetsInputs;
+
+    private void Awake()
+    {
+        starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (starterAssetsInputs.shoot)
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
+            {
+                Debug.Log(hit.collider.name);
+            }
+            starterAssetsInputs.ShootInput(false);
+        }
+
+
+    }
+}
