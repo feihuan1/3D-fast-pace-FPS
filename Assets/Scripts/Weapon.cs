0a743a90d8e0ad3a4e6463b0a87e6602c2a7ae73
Feihuan Peng <ericrealtor1997@gmail.com>
2025-01-26 17:53:02 -0800

    robot eneemator

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,13 +1,18 @@
 using NUnit.Framework.Internal;
 using StarterAssets;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-
+    [SerializeField] GameObject hitVFXPrefab;
+    [SerializeField] Animator animator;
+    [SerializeField] ParticleSystem muzzleFlash;
     [SerializeField] int damageAmount = 1;
 
     StarterAssetsInputs starterAssetsInputs;
+
+    const string SHOOT_STRING = "Shoot";
 
     private void Awake()
     {
@@ -18,21 +23,25 @@ public class Weapon : MonoBehaviour
     void Update()
     {
         handleShoot();
-        starterAssetsInputs.ShootInput(false);
     }
 
     private void handleShoot()
     {
+        
         if (!starterAssetsInputs.shoot) return;
+
+        muzzleFlash.Play();
+        animator.Play(SHOOT_STRING, 0, 0f);
+        starterAssetsInputs.ShootInput(false);
 
         RaycastHit hit;
 
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
         {
+            Instantiate(hitVFXPrefab,hit.point,quaternion.identity);
+            
             EnemyHealth enemyHealth = hit.collider.gameObject.GetComponent<EnemyHealth>();
-
             enemyHealth?.TakeDamage(damageAmount);
-
         }
     }
 }
